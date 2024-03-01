d8ec6bc4a3e09742bd24d0b30a631ddd3e882a08
Aasil Ghoury <152463382+studentaasil@users.noreply.github.com>
2024-02-29 19:56:31 -0800

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const useCors = (app) => {
   app.use(
     cors({
-      origin: "/",
+      origin: "https://teal-vivacious-cygnet.cyclic.app",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
