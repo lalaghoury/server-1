8978848de2bd9d753b0ac98e3e14febf8344412e
Aasil Ghoury <152463382+studentaasil@users.noreply.github.com>
2024-02-29 19:48:00 -0800

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const useCors = (app) => {
   app.use(
     cors({
-      origin: "https://perfectrecipebyaasil.vercel.app",
+      origin: "/",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
